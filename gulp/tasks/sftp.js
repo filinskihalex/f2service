bd1b60d4bd908df741fe80d116982b3f1d2d511a
Alex <filinskihalex@gmail.com>
2021-04-16 13:30:13 +0300

    sql

--- gulp/tasks/sftp.js
+++ gulp/tasks/sftp.js
@@ -5,6 +5,7 @@ module.exports = function ftp(cb) {
     return gulp.src('build/**/*')
         .pipe(sftp({
             host: 'f2service.ru',
+            port: '1711',
             user: 'root',
             remotePath: '/root/docker/src/f2service_ru'
         }));
